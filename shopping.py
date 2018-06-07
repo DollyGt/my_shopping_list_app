6d22338c76e6fe0664bbe837b1d3dfd116d367e4
DollyG <dollsgoitse@yahoo.com>
2018-06-07 13:13:46 +0000

    user and home html

--- shopping.py
+++ shopping.py
@@ -18,20 +18,66 @@ def do_login():
 
 @app.route("/<username>") 
 def get_userpage(username):
-    return render_template("lists.html", username=username)
+    user_lists = load_list_names_from_mongo(username)
+    return render_template("user_home.html", username=username, user_lists=user_lists)
 
  
-@app.route("/<username>/new_list", methods=["POST"]) 
-def add_message(username):
+@app.route("/<username>/create_new_list", methods=["POST"]) 
+def add_list_name(username):
     list_name= request.form['list_name']
+    save_list_names_to_mongo(username,list_name)
     return redirect(username)
     
+@app.route("/<username>/<list_name>", methods=["GET","POST"]) 
+def view_specific_list_by_user(username, list_name):
+    list_items = load_list_items_from_mongo(username, list_name)
+    return render_template("user_lists.html", username=username, list_name=list_name, list_items=list_items)
     
-def save_lists_to_mongo():
-    pass
+@app.route("/<username>/<list_name>/add_item", methods=["POST"]) 
+def add_item_to_list(username, list_name):
+    list_item= request.form['list_item']
+    save_list_items_to_mongo(username, list_name, list_item)
+    return redirect(username + "/" + list_name)
     
-def load_list_names_from_mongo():
-    pass
+def save_list_names_to_mongo(username, list_name):
+    with MongoClient(MONGODB_URI) as conn:
+        db = conn[MONGODB_NAME]
+        collection = db[username]
+        collection.insert({'name': list_name, 'list_items': [] })
+        
+def load_list_names_from_mongo(username):
+    with MongoClient(MONGODB_URI) as conn:
+        db = conn[MONGODB_NAME]
+        collection = db[username]
+        users = db.collection_names()
+        if username in users:
+            user_lists = collection.find()
+            return user_lists
+        
+def save_list_items_to_mongo(username, list_name, new_list_item):
+    with MongoClient(MONGODB_URI) as conn:
+        db = conn[MONGODB_NAME]
+        collection = db[username]
+        selected_list = collection.find({'name':list_name})
+        new_list_of_items = []
+        for i in selected_list:
+            if i["list_items"]:
+                for item in i["list_items"]:
+                    new_list_of_items.append(item)
+            
+        new_list_of_items.append(new_list_item)
+        collection.find_one_and_update({"name": list_name}, {"$set": {"list_items": new_list_of_items}})
+
+def load_list_items_from_mongo(username, list_name):
+    with MongoClient(MONGODB_URI) as conn:
+        db = conn[MONGODB_NAME]
+        collection = db[username]
+        users = db.collection_names()
+        if username in users:
+            list_items = collection.find({'name':list_name})
+            return list_items
+        
+
 
 
 
